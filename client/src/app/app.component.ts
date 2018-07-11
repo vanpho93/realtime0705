9c7293ae0a004c2a305f4a9412fd4885f427befb
Van Pho Nguyen <vanpho02@gmail.com>
2018-07-11 20:13:35 +0700

    app

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
+import io from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   txtMessage = '';
+  socket = io('https://realtime0705.herokuapp.com/');
+
+  constructor() {
+    this.socket.on('SERVER_SEND_MESSAGE', msg => console.log(msg));
+  }
 
   sendMessage() {
-    alert('New Message: ' + this.txtMessage);
+    this.socket.emit('CLIENT_SEND_MESSAGE', this.txtMessage);
     this.txtMessage = '';
   }
 }
